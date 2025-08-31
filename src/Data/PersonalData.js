47087cf92983cdcf261839f6f13b5a469e04ef07
vijay <your.email@example.com>
2025-08-31 20:09:44 +0530

    Fix resume link changes

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -4,8 +4,8 @@ const PersonalData = {
     mobNo:"+91 8220233838",
     email:"vijayaravindan1065@gmail.com",
     address:"India",
-    aboutMe:"Hi Everyone, I am VIJAY ARAVINDAN from Dindugal Tamil Nadu,India. I am a passionate Frontend Developer with strong skills in React.js, JavaScript, HTML, CSS, and Tailwind CSS. I enjoy creating modern, responsive, and user-friendly web applications with a focus on clean design and smooth user experience. I am also familiar with Git and GitHub for version control and teamwork. Currently, I am expanding my knowledge in the MERN stack to strengthen my full-stack development skills. I love solving problems, building creative projects, and continuously learning new technologies to grow as a developer.",
-    resumeLink:'https://drive.google.com/file/d/1CHfLFZ-2zA0dABOadIw5zSoDouPUJI3T/view?usp=sharing',
+    aboutMe:"Hi Everyone, I am VIJAY ARAVINDAN from Dindigul Tamil Nadu,India. I am a passionate Frontend Developer with strong skills in React.js, JavaScript, HTML, CSS, and Tailwind CSS. I enjoy creating modern, responsive, and user-friendly web applications with a focus on clean design and smooth user experience. I am also familiar with Git and GitHub for version control and teamwork. Currently, I am expanding my knowledge in the MERN stack to strengthen my full-stack development skills. I love solving problems, building creative projects, and continuously learning new technologies to grow as a developer.",
+    resumeLink:'https://drive.google.com/file/d/1f_f5kmAnO7q8lhe8leGslZDAjUZJTGMb/view?usp=sharing',
     imageUrl:''
 }
 export const autoTypeData=['Frontend Developer !'];
--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -3,9 +3,9 @@ const DUMMY_PROJECTS = [
     projectTitle: 'PortFolio Template',
     description:
       'This project is a React app to generate a creative professional website based on Data provided. The website uses no backend and is free to fork and use.',
-    sourceLink: 'https://github.com/killer-abhi/Portfolio.git',
-    liveLink: 'https://your-portfolio.netlify.app',  // <-- Netlify link
-    lastUpdated: 'Jan 20 2023'
+    sourceLink: 'https://github.com/aravindan345/Portfolio',
+    liveLink: 'https://portfoliovij.netlify.app',  // <-- Netlify link
+    lastUpdated: ' Mar 20 2025'
   },
   {
     projectTitle: 'Vijay Shopping E-Commerce',
